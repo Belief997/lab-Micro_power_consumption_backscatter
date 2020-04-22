ea02f29d1177ef3806ed00e239df47f3406699a2
belief997 <xieyr997@gmail.com>
2020-04-23 04:07:36 +0800

    房外tag衰减模型实现，准确率提高到0.94

--- matlab/SinglePoint2.m
+++ matlab/SinglePoint2.m
@@ -1,10 +1,10 @@
-function [] = single_run(r_tagIn,r_tagOut, r_direct, draw_plot, type)
+function [] = SinglePoint2(x, y, r_tagIn,r_tagOut, r_direct, draw_plot, type)
 %SINGLE_RUN 对任意点的衰减和延时计算，以及波形绘制
 %   
 global c;
 global ratio;
 global f_source;
-global P_source;
+% global P_source;
 global y_source;
 global y_sig;
 global fs;
@@ -25,7 +25,7 @@ f_tx = f_source * ratio;
 G_source = zeros(size(r_tagIn));
 G_tag = zeros(size(r_tagIn));
 L_tagIn = ideal_decline(f_tx * 10^(-6), r_tagIn, G_source, G_tag);
-P_tagIn = P_source .* 10.^(-0.1 * L_tagIn);
+% P_tagIn = P_source .* 10.^(-0.1 * L_tagIn);
 
 % 直射以及 tag 之后，接收来自直射路径和 tag 散射的衰减和接收功率
 % r_tagOut = 3.25;
@@ -33,24 +33,30 @@ P_tagIn = P_source .* 10.^(-0.1 * L_tagIn);
 G_rx = 0;
 L_tagOut = ideal_decline(f_tx * 10^(-6), r_tagOut, G_tag, G_rx);
 L_direct = ideal_decline(f_tx * 10^(-6), r_direct, G_source, G_rx);
-P_tag = P_tagIn .* 10.^(-0.1 * L_tagOut);
-P_direct = P_source .* 10.^(-0.1 * L_direct);
+% P_tag = P_tagIn .* 10.^(-0.1 * L_tagOut);
+% P_direct = P_source .* 10.^(-0.1 * L_direct);
+
+% 墙外衰减
+L_t = outsideDecline(x, y, L_tagIn, L_tagOut);
 
 % 保存数据
-if type == 'beacon'
+if strcmp(type , 'beacon')
     filename = sprintf('%sBeacon_%s', dirname, 'decline');
-else if type == 'tag'
+elseif strcmp(type ,  'tag')
     filename = sprintf('%s%s', dirname, 'decline');
+elseif strcmp(type ,  'inside')
+    filename = sprintf('.\\DataSet\\i_%s', 'decline');
+elseif strcmp(type ,  'outside')
+    filename = sprintf('.\\DataSet\\o_%s', 'decline');
 end
-save(filename, 'L_tagIn', 'L_tagOut', 'L_direct');
+save(filename, 'L_t', 'L_direct');
 
-% 使用 tag 前后的距离和计算衰减，不采用
-% L_test = ideal_decline(f_tx * 10^(-6), r_tagIn + r_tagOut, G_source, G_rx);
-% P_test = P_source * 10^(-0.1 * L_test); 
+
 %--------------------------------------
 % 分别画出两条路径衰减后的信号
-decline_direct = y_source .* repmat(reshape((P_direct ./ P_source)', [], 1), 1, length(y_source));
-decline_tag = y_sig .* repmat(reshape((P_tag ./ P_source)', [], 1), 1, length(y_sig));
+% 按行转成列向量，复制列
+decline_direct = y_source .* repmat(reshape((10.^(-0.1 * L_direct))', [], 1), 1, length(y_source));
+decline_tag = y_sig .* repmat(reshape((10.^(-0.1 * L_t))', [], 1), 1, length(y_sig));
 
 if draw_plot
     % decline_tag = y_sig * (P_test / P_source);
@@ -73,10 +79,14 @@ disp('delta t is');
 disp(delta_t);
 
 % 保存数据
-if type
+if strcmp(type , 'beacon')
     filename = sprintf('%sBeacon_%s', dirname, 'delay');
-else
+elseif strcmp(type , 'tag')
     filename = sprintf('%s%s', dirname, 'delay');  
+elseif strcmp(type ,  'inside')
+    filename = sprintf('.\\DataSet\\i_%s', 'delay');
+elseif strcmp(type ,  'outside')
+    filename = sprintf('.\\DataSet\\o_%s', 'delay');
 end
 save(filename, 'delay_reflect', 'delay_direct');
 
@@ -92,10 +102,14 @@ save(filename, 'delay_reflect', 'delay_direct');
     sig_tag = [shift_reflect decline_tag(i,:)];
     sig_rx = sig_dir + sig_tag;
     % 保存数据
-    if type
-        filename = sprintf('%sBeacon_%s_%s', dirname, 'sig_rx', num2str(i));
-    else
-        filename = sprintf('%s%s_%s', dirname, 'sig_rx', num2str(i));
+    if strcmp(type ,  'beacon')
+        filename = sprintf('%sBeacon_%s_%d', dirname, 'sig_rx', i);
+    elseif strcmp(type ,  'tag')
+        filename = sprintf('%s%s_%d', dirname, 'sig_rx', i);
+    elseif strcmp(type , 'inside')
+        filename = sprintf('.\\DataSet\\i_%s_%d', 'sig_rx', i);
+    elseif strcmp(type ,  'outside')
+        filename = sprintf('.\\DataSet\\o_%s_%d', 'sig_rx', i);
     end
     save(filename, 'sig_dir', 'sig_tag', 'sig_rx');
         
@@ -117,11 +131,6 @@ save(filename, 'delay_reflect', 'delay_direct');
         title('叠加');
     end
 end
-
-%--------------------------------------
-
-
-
 
 
 end
