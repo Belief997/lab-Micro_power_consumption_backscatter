d627257499828e0cf18b9df604d45078d4ee77a0
belief997 <xieyr997@gmail.com>
2020-07-21 19:07:38 +0800

    暂存，出现调试问题

--- SDK_2.3.1_MKL03Z32xxx4/boards/frdmkl03z/driver_examples/lpuart/interrupt_transfer/lpuart_interrupt_transfer.c
+++ SDK_2.3.1_MKL03Z32xxx4/boards/frdmkl03z/driver_examples/lpuart/interrupt_transfer/lpuart_interrupt_transfer.c
@@ -40,6 +40,9 @@
 
 #include "user_func.h"
 
+#include "fsl_lptmr.h"
+#include "fsl_gpio.h"
+
 
 extern lpuart_handle_t g_lpuartHandle;
 
@@ -53,6 +56,52 @@ extern volatile bool txBufferFull ;
 extern volatile bool txOnGoing ;
 extern volatile bool rxOnGoing ;
 
+
+
+
+
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define DEMO_LPTMR_BASE LPTMR0
+#define DEMO_LPTMR_IRQn LPTMR0_IRQn
+#define LPTMR_LED_HANDLER LPTMR0_IRQHandler
+/* Get source clock for LPTMR driver */
+#define LPTMR_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_LpoClk)
+/* Define LPTMR microseconds counts value */
+//#define LPTMR_USEC_COUNT 1000000U
+#define LPTMR_USEC_COUNT 1000U
+
+#define LED_INIT() LED_RED_INIT(LOGIC_LED_ON)
+#define LED_TOGGLE() LED_RED_TOGGLE()
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+volatile uint32_t lptmrCounter = 0U;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void LPTMR_LED_HANDLER(void)
+{
+    LPTMR_ClearStatusFlags(DEMO_LPTMR_BASE, kLPTMR_TimerCompareFlag);
+    lptmrCounter++;
+    LED_TOGGLE();
+    /*
+     * Workaround for TWR-KV58: because write buffer is enabled, adding
+     * memory barrier instructions to make sure clearing interrupt flag completed
+     * before go out ISR
+     */
+    __DSB();
+    __ISB();
+}
 
 
 
@@ -71,69 +120,114 @@ int main(void)
     CLOCK_SetLpuart0Clock(0x1U);
 
     dac_init();
-    dac_test();
+//    dac_test();
 
 
+// TIMER
+    uint32_t currentCounter = 0U;
+    lptmr_config_t lptmrConfig;
 
+    LED_INIT();
 
+    /* Board pin, clock, debug console init */
+//    BOARD_InitPins();
+//    BOARD_BootClockRUN();
+//    BOARD_InitDebugConsole();
 
+    /* Configure LPTMR */
+    /*
+     * lptmrConfig.timerMode = kLPTMR_TimerModeTimeCounter;
+     * lptmrConfig.pinSelect = kLPTMR_PinSelectInput_0;
+     * lptmrConfig.pinPolarity = kLPTMR_PinPolarityActiveHigh;
+     * lptmrConfig.enableFreeRunning = false;
+     * lptmrConfig.bypassPrescaler = true;
+     * lptmrConfig.prescalerClockSource = kLPTMR_PrescalerClock_1;
+     * lptmrConfig.value = kLPTMR_Prescale_Glitch_0;
+     */
+    LPTMR_GetDefaultConfig(&lptmrConfig);
+//    lptmrConfig¡£prescalerClockSource = kLPTMR_PrescalerClock_0;
+
+    /* Initialize the LPTMR */
+    LPTMR_Init(DEMO_LPTMR_BASE, &lptmrConfig);
 
     /*
-     * config.baudRate_Bps = 115200U;
-     * config.parityMode = kLPUART_ParityDisabled;
-     * config.stopBitCount = kLPUART_OneStopBit;
-     * config.txFifoWatermark = 0;
-     * config.rxFifoWatermark = 0;
-     * config.enableTx = false;
-     * config.enableRx = false;
-     */
-    LPUART_GetDefaultConfig(&config);
-    config.baudRate_Bps = BOARD_DEBUG_UART_BAUDRATE;
-    config.enableTx = true;
-    config.enableRx = true;
+     * Set timer period.
+     * Note : the parameter "ticks" of LPTMR_SetTimerPeriod should be equal or greater than 1.
+    */
+    LPTMR_SetTimerPeriod(DEMO_LPTMR_BASE, USEC_TO_COUNT(LPTMR_USEC_COUNT, LPTMR_SOURCE_CLOCK));
 
-    LPUART_Init(DEMO_LPUART, &config, DEMO_LPUART_CLK_FREQ);
-    LPUART_TransferCreateHandle(DEMO_LPUART, &g_lpuartHandle, LPUART_UserCallback, NULL);
+    /* Enable timer interrupt */
+    LPTMR_EnableInterrupts(DEMO_LPTMR_BASE, kLPTMR_TimerInterruptEnable);
 
-    /* Send g_tipString out. */
-    xfer.data = g_tipString;
-    xfer.dataSize = sizeof(g_tipString) - 1;
-    txOnGoing = true;
-    LPUART_TransferSendNonBlocking(DEMO_LPUART, &g_lpuartHandle, &xfer);
+    /* Enable at the NVIC */
+    EnableIRQ(DEMO_LPTMR_IRQn);
 
-    /* Wait send finished */
-    while (txOnGoing)
-    {
-    }
+//    PRINTF("Low Power Timer Example\r\n");
 
-    /* Start to echo. */
-    sendXfer.data = g_txBuffer;
-    sendXfer.dataSize = ECHO_BUFFER_LENGTH;
-    receiveXfer.data = g_rxBuffer;
-    receiveXfer.dataSize = ECHO_BUFFER_LENGTH;
+    /* Start counting */
+    LPTMR_StartTimer(DEMO_LPTMR_BASE);
 
-    while (1)
-    {
-        /* If RX is idle and g_rxBuffer is empty, start to read data to g_rxBuffer. */
-        if ((!rxOnGoing) && rxBufferEmpty)
-        {
-            rxOnGoing = true;
-            LPUART_TransferReceiveNonBlocking(DEMO_LPUART, &g_lpuartHandle, &receiveXfer, NULL);
-        }
 
-        /* If TX is idle and g_txBuffer is full, start to send data. */
-        if ((!txOnGoing) && txBufferFull)
-        {
-            txOnGoing = true;
-            LPUART_TransferSendNonBlocking(DEMO_LPUART, &g_lpuartHandle, &sendXfer);
-        }
 
-        /* If g_txBuffer is empty and g_rxBuffer is full, copy g_rxBuffer to g_txBuffer. */
-        if ((!rxBufferEmpty) && (!txBufferFull))
-        {
-            memcpy(g_txBuffer, g_rxBuffer, ECHO_BUFFER_LENGTH);
-            rxBufferEmpty = true;
-            txBufferFull = true;
-        }
-    }
+
+
+
+//    /*
+//     * config.baudRate_Bps = 115200U;
+//     * config.parityMode = kLPUART_ParityDisabled;
+//     * config.stopBitCount = kLPUART_OneStopBit;
+//     * config.txFifoWatermark = 0;
+//     * config.rxFifoWatermark = 0;
+//     * config.enableTx = false;
+//     * config.enableRx = false;
+//     */
+//    LPUART_GetDefaultConfig(&config);
+//    config.baudRate_Bps = BOARD_DEBUG_UART_BAUDRATE;
+//    config.enableTx = true;
+//    config.enableRx = true;
+//
+//    LPUART_Init(DEMO_LPUART, &config, DEMO_LPUART_CLK_FREQ);
+//    LPUART_TransferCreateHandle(DEMO_LPUART, &g_lpuartHandle, LPUART_UserCallback, NULL);
+//
+//    /* Send g_tipString out. */
+//    xfer.data = g_tipString;
+//    xfer.dataSize = sizeof(g_tipString) - 1;
+//    txOnGoing = true;
+//    LPUART_TransferSendNonBlocking(DEMO_LPUART, &g_lpuartHandle, &xfer);
+//
+//    /* Wait send finished */
+//    while (txOnGoing)
+//    {
+//    }
+//
+//    /* Start to echo. */
+//    sendXfer.data = g_txBuffer;
+//    sendXfer.dataSize = ECHO_BUFFER_LENGTH;
+//    receiveXfer.data = g_rxBuffer;
+//    receiveXfer.dataSize = ECHO_BUFFER_LENGTH;
+//
+//    while (1)
+//    {
+//        /* If RX is idle and g_rxBuffer is empty, start to read data to g_rxBuffer. */
+//        if ((!rxOnGoing) && rxBufferEmpty)
+//        {
+//            rxOnGoing = true;
+//            LPUART_TransferReceiveNonBlocking(DEMO_LPUART, &g_lpuartHandle, &receiveXfer, NULL);
+//        }
+//
+//        /* If TX is idle and g_txBuffer is full, start to send data. */
+//        if ((!txOnGoing) && txBufferFull)
+//        {
+//            txOnGoing = true;
+//            LPUART_TransferSendNonBlocking(DEMO_LPUART, &g_lpuartHandle, &sendXfer);
+//        }
+//
+//        /* If g_txBuffer is empty and g_rxBuffer is full, copy g_rxBuffer to g_txBuffer. */
+//        if ((!rxBufferEmpty) && (!txBufferFull))
+//        {
+//            memcpy(g_txBuffer, g_rxBuffer, ECHO_BUFFER_LENGTH);
+//            rxBufferEmpty = true;
+//            txBufferFull = true;
+//        }
+//    }
 }
