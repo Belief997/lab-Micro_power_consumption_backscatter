4340fed6f92a1b49c50ac65643600171f96d407e
belief997 <xieyr997@gmail.com>
2020-08-18 21:48:53 +0800

    清理无用的sdk

--- KinetisSDK/examples/frdmkl03z/user_apps/Prj_test16/kds/debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := ../../../../../../lib/ksdk_platform_lib/kds/KL03Z4/debug/libksdk_platform.a
-
-LIBS := -lm -lg -lgcc -lnosys
-
--- MAPS-KL03-KSDK_1.0.0/demos/low_power_rtc/uv4/mapskl03/debug/startup_mkl03z4.d
+++
@@ -1,1 +0,0 @@
-debug\startup_mkl03z4.o: ../../../../platform/startup/MKL03Z4/arm/startup_MKL03Z4.s
