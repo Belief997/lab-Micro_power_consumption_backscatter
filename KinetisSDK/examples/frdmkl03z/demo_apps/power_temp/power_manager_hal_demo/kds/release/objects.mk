f13b056b8a32f9d9218c26ec7d82b0e60e784d5a
belief997 <xieyr997@gmail.com>
2020-09-07 17:37:18 +0800

    清除无用的文件

--- KinetisSDK/examples/frdmkl03z/demo_apps/power_temp/power_manager_hal_demo/kds/release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := E:/Project/IAR/MPC_backscatter/lab-Micro_power_consumption_backscatter/KinetisSDK/examples/frdmkl03z/demo_apps/power_manager_hal_demo/kds/../../../../../lib/ksdk_platform_lib/kds/KL03Z4/release/libksdk_platform.a
-
-LIBS := -lm -lg -lgcc -lnosys
-
--- MAPS-KL03-KSDK_1.0.0/demos/low_power_rtc/uv4/mapskl03/debug/fsl_misc_utilities.d
+++
@@ -1,6 +0,0 @@
-debug\fsl_misc_utilities.o: ../../../../platform/utilities/src/fsl_misc_utilities.c
-debug\fsl_misc_utilities.o: D:\29446\MDK514\ARM\ARMCC\Bin\..\include\stdarg.h
-debug\fsl_misc_utilities.o: D:\29446\MDK514\ARM\ARMCC\Bin\..\include\stdio.h
-debug\fsl_misc_utilities.o: D:\29446\MDK514\ARM\ARMCC\Bin\..\include\stdlib.h
-debug\fsl_misc_utilities.o: ../../../../platform/utilities/fsl_misc_utilities.h
-debug\fsl_misc_utilities.o: D:\29446\MDK514\ARM\ARMCC\Bin\..\include\stdint.h
